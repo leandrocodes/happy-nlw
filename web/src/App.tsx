7033b9930815819ea784d861e61febab19cc5b8e
leandrocodes <leandrovianacodes@gmail.com>
2020-10-12 15:47:01 -0400

    :construction: using react-router-dom

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,10 @@
 import React from 'react'
-import { FiArrowRight } from 'react-icons/fi'
 import './styles/global.css'
-import './styles/pages/landing.css'
 
-import logoImg from './images/logo.svg'
+import Routes from './Routes'
 
 function App() {
-  return (
-    <div id='page-landing'>
-      <div className='content-wrapper'>
-        <img src={logoImg} alt='Happy' />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className='location'>
-          <strong>Ponta Porã</strong>
-          <span>Mato Grosso do Sul</span>
-        </div>
-
-        <a href='./' className='enter-app'>
-          <FiArrowRight size={26} color='rgba(0, 0, 0, 0.5)' />
-        </a>
-      </div>
-    </div>
-  )
+  return <Routes />
 }
 
 export default App
