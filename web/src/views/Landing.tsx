277775b7a9d83a02e94d9bbf31b152610e4834bb
leandrocodes <leandrovianacodes@gmail.com>
2020-10-12 15:50:43 -0400

    :construction: using Link to routing

--- web/src/views/Landing.tsx
+++ web/src/views/Landing.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FiArrowRight } from 'react-icons/fi'
 
+import { Link } from 'react-router-dom'
+
 import '../styles/pages/landing.css'
 
 import logoImg from '../images/logo.svg'
@@ -21,9 +23,9 @@ function Landing() {
           <span>Mato Grosso do Sul</span>
         </div>
 
-        <a href='/app' className='enter-app'>
+        <Link to='/app' className='enter-app'>
           <FiArrowRight size={26} color='rgba(0, 0, 0, 0.5)' />
-        </a>
+        </Link>
       </div>
     </div>
   )
