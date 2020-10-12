e6a7a59bb29186084ab6bcf40a2ba9efe1fc2a24
leandrocodes <leandrovianacodes@gmail.com>
2020-10-12 15:28:34 -0400

    :art: landing page :rocket:

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,30 @@
 import React from 'react'
-
+import { FiArrowRight } from 'react-icons/fi'
 import './styles/global.css'
+import './styles/pages/landing.css'
 
-interface TitleProps {
-  text: string
-}
-
-function Title(props: TitleProps) {
-  return <h1>{props.text}</h1>
-}
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className='App'>
-      <Title text='Title 1' />
-      <Title text='Title 2' />
-      <Title text='Title 3' />
-      <Title text='Title 4' />
+    <div id='page-landing'>
+      <div className='content-wrapper'>
+        <img src={logoImg} alt='Happy' />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className='location'>
+          <strong>Ponta Porã</strong>
+          <span>Mato Grosso do Sul</span>
+        </div>
+
+        <a href='./' className='enter-app'>
+          <FiArrowRight size={26} color='rgba(0, 0, 0, 0.5)' />
+        </a>
+      </div>
     </div>
   )
 }
