b2819e29de00f94261e5352a75be22b141e6e4f4
leandrocodes <leandrovianacodes@gmail.com>
2020-10-13 17:21:32 -0400

    :construction: update migration

---
+++ backend/src/models/Orphanage.ts
@@ -0,0 +1,10 @@
+export default class Ophanage {
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+  about: string
+  instructions: string
+  opening_hours: string
+  open_on_weekends: boolean
+}
