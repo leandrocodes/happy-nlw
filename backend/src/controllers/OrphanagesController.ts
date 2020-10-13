222ebea16d316ae88e452d2ead2515961fd421cb
leandrocodes <leandrovianacodes@gmail.com>
2020-10-13 18:07:01 -0400

    :construction: Orphanage Controller

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from '../models/Orphanage'
+
+export default {
+  async create(req: Request, res: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = req.body
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    })
+
+    await orphanagesRepository.save(orphanage)
+
+    return res.status(201).json(orphanage)
+  },
+
+  async index(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanages = await orphanagesRepository.find()
+
+    return res.json(orphanages)
+  }
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,36 +1,8 @@
-import { getRepository } from 'typeorm'
-import Orphanage from './models/Orphanage'
-
 import { Router } from 'express'
-
+import OrphanagesController from './controllers/OrphanagesController'
 const routes = Router()
 
-routes.post('/orphanages', async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = req.body
-
-  const orphanagesRepository = getRepository(Orphanage)
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  })
-
-  await orphanagesRepository.save(orphanage)
-
-  return res.status(201).json(orphanage)
-})
+routes.post('/orphanages', OrphanagesController.create)
+routes.get('/orphanages', OrphanagesController.index)
 
 export default routes
