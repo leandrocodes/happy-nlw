88369cedd7290ed81c465b411f08509c760a1004
leandrocodes <leandrovianacodes@gmail.com>
2020-10-13 17:57:06 -0400

    :tree: separating route files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,10 @@
 import express from 'express'
-import { getRepository } from 'typeorm'
-import Orphanage from './models/Orphanage'
 
 import './db/connection'
 
+import routes from './routes'
+
 const app = express()
 app.use(express.json())
-
-app.post('/orphanages', async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = req.body
-
-  const orphanagesRepository = getRepository(Orphanage)
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  })
-
-  await orphanagesRepository.save(orphanage)
-
-  return res.json({ message: 'Hello World' })
-})
-
+app.use(routes)
 app.listen(3030)
