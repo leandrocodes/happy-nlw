489c808fe7c9dd4f8c49a0580b945c3e62d7d1c7
leandrocodes <leandrovianacodes@gmail.com>
2020-10-13 19:37:40 -0400

    :construction: validation errors with yup

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,21 @@
 import { ErrorRequestHandler } from 'express'
+import { ValidationError } from 'yup'
+
+interface ValidationErrors {
+  [key: string]: string[]
+}
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    let errors: ValidationErrors = {}
+
+    err.inner.forEach(err => {
+      errors[err.path] = err.errors
+    })
+
+    return res.status(400).json({ message: 'Validation Error', errors })
+  }
+
   console.error(err)
 
   return res.status(500).json({ message: 'Internal Server Error' })
