d9af1063b29d15b4108afabb42d3ecf85977187f
leandrocodes <leandrovianacodes@gmail.com>
2020-10-13 17:46:49 -0400

    :construction: orphanages post route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,37 @@
 import express from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from './models/Orphanage'
 
 import './db/connection'
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
+app.post('/orphanages', async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = req.body
+
+  const orphanagesRepository = getRepository(Orphanage)
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  })
+
+  await orphanagesRepository.save(orphanage)
+
   return res.json({ message: 'Hello World' })
 })
 
