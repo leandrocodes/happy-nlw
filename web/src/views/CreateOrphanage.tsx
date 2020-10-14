b648f1c304d2d71cd4c9fbe2bafc74b524ff9f24
leandrocodes <leandrovianacodes@gmail.com>
2020-10-14 13:25:31 -0400

    :sparkles: componentization

--- web/src/views/CreateOrphanage.tsx
+++ web/src/views/CreateOrphanage.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import { useHistory } from "react-router-dom";
+import React from 'react'
+import { Map, Marker, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
+import { FiPlus } from 'react-icons/fi'
 
-import mapMarkerImg from '../images/map-marker.svg';
+import mapMarkerImg from '../images/map-marker.svg'
 
-import '../styles/pages/create-orphanage.css';
+import '../styles/pages/create-orphanage.css'
+
+import Sidebar from '../components/Sidebar'
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
@@ -18,56 +19,49 @@ const happyMapIcon = L.icon({
 })
 
 export default function CreateOrphanage() {
-  const { goBack } = useHistory();
-
   return (
-    <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
-
+    <div id='page-create-orphanage'>
+      <Sidebar />
       <main>
-        <form className="create-orphanage-form">
+        <form className='create-orphanage-form'>
           <fieldset>
             <legend>Dados</legend>
 
-            <Map 
-              center={[-27.2092052,-49.6401092]} 
+            <Map
+              center={[-27.2092052, -49.6401092]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
             >
-              <TileLayer 
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+              <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              <Marker
+                interactive={false}
+                icon={happyMapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
             </Map>
 
-            <div className="input-block">
-              <label htmlFor="name">Nome</label>
-              <input id="name" />
+            <div className='input-block'>
+              <label htmlFor='name'>Nome</label>
+              <input id='name' />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
+            <div className='input-block'>
+              <label htmlFor='about'>
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
+              <textarea id='name' maxLength={300} />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="images">Fotos</label>
+            <div className='input-block'>
+              <label htmlFor='images'>Fotos</label>
 
-              <div className="uploaded-image">
+              <div className='uploaded-image'></div>
 
-              </div>
-
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
+              <button className='new-image'>
+                <FiPlus size={24} color='#15b6d6' />
               </button>
             </div>
           </fieldset>
@@ -75,33 +69,35 @@ export default function CreateOrphanage() {
           <fieldset>
             <legend>Visitação</legend>
 
-            <div className="input-block">
-              <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+            <div className='input-block'>
+              <label htmlFor='instructions'>Instruções</label>
+              <textarea id='instructions' />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+            <div className='input-block'>
+              <label htmlFor='opening_hours'>Nome</label>
+              <input id='opening_hours' />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+            <div className='input-block'>
+              <label htmlFor='open_on_weekends'>Atende fim de semana</label>
 
-              <div className="button-select">
-                <button type="button" className="active">Sim</button>
-                <button type="button">Não</button>
+              <div className='button-select'>
+                <button type='button' className='active'>
+                  Sim
+                </button>
+                <button type='button'>Não</button>
               </div>
             </div>
           </fieldset>
 
-          <button className="confirm-button" type="submit">
+          <button className='confirm-button' type='submit'>
             Confirmar
           </button>
         </form>
       </main>
     </div>
-  );
+  )
 }
 
 // return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
